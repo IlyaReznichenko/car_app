b371f45ff12f7315680f8a8592aabc2b9818bd81
IlyaReznichenko <chriscolesb@gmail.com>
2022-08-31 21:41:57 +0300

    upload xml file v1.2

---
+++ mainapp/excel_handler.py
@@ -0,0 +1,24 @@
+import openpyxl
+
+class UploadExcel(object):
+    def __init__(self, file):
+        self.file = file
+        if isinstance(self.file, object):
+            self.excel_parser()
+        else:
+            print('Неверно передан объект')
+
+    def excel_parser(self):
+        open_excel_file = openpyxl.load_workbook(self.file)
+        excel_sheet = open_excel_file["Sheet1"]
+        excel_data = []
+
+        for row in excel_sheet.iter_rows():
+            row_data = []
+            for cell in row:
+                row_data.append(str(cell.value))
+            excel_data.append(row_data)
+
+        self.excel_data = excel_data
+
+
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .excel_handler import UploadExcel
 
 
 def main_page(request):
     if request.POST:
-        print(request.POST)
-        print(request.FILES)
+        # print(request.POST)
+        # print(request.FILES)
+        file = request.FILES['excel_file']
+        upload_excel = UploadExcel(file)
     return render(request, 'mainapp/index.html')
